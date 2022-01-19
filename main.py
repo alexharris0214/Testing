24c5d11393d7019fd80d2ef1d42cb2130fb45ce3
alexharris0214 <alexharris0214@gmail.com>
2022-01-19 18:55:46 -0500

    new rect changes

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 class Rect:
 
-    def __init__(x, y):
+    def __init__(self, x, y):
         self.x = x
         self.y = y
 
+    def increment_x(self, x):
+        self.x +=x
+
+    def increment_y(self, x):
+        self.x +=1
 
 x = Rect(3,4)
+x.increment_x(3)
+x.__init__(8,7)
+print(x.x)
+
